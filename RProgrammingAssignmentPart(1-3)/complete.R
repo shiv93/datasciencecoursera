1f914a45aa4cd23e6c227e2c1a77daee3f9d3439
shiv93 <shivangshu.nag2012@vit.ac.in>
2015-04-26 01:48:50 +0530

    Added the description of the working inside the comments

--- RProgrammingAssignmentPart(1-3)/complete.R
+++ RProgrammingAssignmentPart(1-3)/complete.R
@@ -1,3 +1,5 @@
+# function reads a directory full of files and reports the number of completely observed cases in each data file. The function returns a data frame where the first column is the name of the file and the second column is the number of complete cases
+
 complete<-function(directory,id=1:332){
   
   count<-0
--- RProgrammingAssignmentPart(1-3)/corr.R
+++ RProgrammingAssignmentPart(1-3)/corr.R
@@ -1,3 +1,8 @@
+#function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. 
+#The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0
+
+
+
 source("complete.R")  #needed when we are calling another function from current function
 corr<- function(directory,threshold=0){
   
@@ -20,7 +25,7 @@ corr<- function(directory,threshold=0){
       file<-read.csv(dir)
       cr<-cor(file$sulfate,file$nitrate,use="complete.obs")
       cr <- round(cr, digits=4)
-      corr<-c(corr,cr)               #nice way to repeatedly add values in a vector
+      corr<-c(corr,cr)               # repeatedly add values in a vector
     }
     
     #temp<-complete.cases(file)
